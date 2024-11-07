a35bb19bd1975e8cc0fed9db1bb74fbbc47be2c1
Artur Arantes <arturarantesads@gmail.com>
2024-11-07 17:35:02 -0300

    Add wrappers for flak-login functions

--- flask_ezlogin/decorators.py
+++ flask_ezlogin/decorators.py
@@ -2,24 +2,33 @@ from functools import wraps
 from flask import redirect, url_for, make_response
 from flask_login import current_user
 
-def verifica_autenticacao(redirect_route="protected_route"):
-    """Decorator que redireciona usuários autenticados para a rota especificada."""
+
+def check_authentication(redirect_route):
+    """Decorator that redirects authenticated users to the specified route."""
+
     def decorator(func):
         @wraps(func)
         def decorated_view(*args, **kwargs):
             if current_user.is_authenticated:
                 return redirect(url_for(redirect_route))
             return func(*args, **kwargs)
+
         return decorated_view
+
     return decorator
 
-def evitar_cache(func):
-    """Decorator que adiciona cabeçalhos para evitar cache na resposta."""
+
+def prevent_cache(func):
+    """Decorator that adds headers to prevent caching of the response."""
+
     @wraps(func)
     def decorated_view(*args, **kwargs):
         response = make_response(func(*args, **kwargs))
-        response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, max-age=0'
-        response.headers['Pragma'] = 'no-cache'
-        response.headers['Expires'] = '-1'
+        response.headers["Cache-Control"] = (
+            "no-store, no-cache, must-revalidate, max-age=0"
+        )
+        response.headers["Pragma"] = "no-cache"
+        response.headers["Expires"] = "-1"
         return response
+
     return decorated_view
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="flask-ezlogin",
-    version="0.1.0",
+    version="0.2.1",
     description="Easy login setup with Flask and Flask-Login",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
@@ -10,10 +10,7 @@ setup(
     author_email="arturarantesads@gmail.com",
     url="https://github.com/arturads/flask-ezlogin",
     packages=find_packages(),
-    install_requires=[
-        "Flask",
-        "Flask-Login"
-    ],
+    install_requires=["Flask", "Flask-Login"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "Framework :: Flask",
